ef46853838caa6fc3114a9d5a868713e2a05bb2e
xanxan2 <xanneteneza@gmail.com>
2020-11-24 19:00:56 +0800

    Updates

---
+++ precache-manifest.322ae941596b75ca66b3d51c45504232.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1712a3a8b2cdf2e3f23e2bcbe43d5d",
+    "url": "/xanxan2/assetclienttest.git/index.html"
+  },
+  {
+    "revision": "5d85b0e36969e3d1b004",
+    "url": "/xanxan2/assetclienttest.git/static/css/0.3bc40ead.chunk.css"
+  },
+  {
+    "revision": "ceca13647159ba8a4ea2",
+    "url": "/xanxan2/assetclienttest.git/static/css/1.b26df3bb.chunk.css"
+  },
+  {
+    "revision": "e45432fc57de4045c44e",
+    "url": "/xanxan2/assetclienttest.git/static/css/main.1b12d644.chunk.css"
+  },
+  {
+    "revision": "5d85b0e36969e3d1b004",
+    "url": "/xanxan2/assetclienttest.git/static/js/0.a4627598.chunk.js"
+  },
+  {
+    "revision": "50657dd44cde48d36e24db270ce065f0",
+    "url": "/xanxan2/assetclienttest.git/static/js/0.a4627598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceca13647159ba8a4ea2",
+    "url": "/xanxan2/assetclienttest.git/static/js/1.3fa32c6a.chunk.js"
+  },
+  {
+    "revision": "d8d8e1a461bc247fbd37ad846147df80",
+    "url": "/xanxan2/assetclienttest.git/static/js/1.3fa32c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f646cf645068d2009eb",
+    "url": "/xanxan2/assetclienttest.git/static/js/10.6b1ce22c.chunk.js"
+  },
+  {
+    "revision": "25c6df908e5fb8621253",
+    "url": "/xanxan2/assetclienttest.git/static/js/11.55168daf.chunk.js"
+  },
+  {
+    "revision": "7066fdd60f4a4ac947a9",
+    "url": "/xanxan2/assetclienttest.git/static/js/12.a6f3f318.chunk.js"
+  },
+  {
+    "revision": "d034a506d059fc3f3dcd",
+    "url": "/xanxan2/assetclienttest.git/static/js/13.07f29d40.chunk.js"
+  },
+  {
+    "revision": "f47ba87aab35cbd307dc",
+    "url": "/xanxan2/assetclienttest.git/static/js/14.0b7a9fe7.chunk.js"
+  },
+  {
+    "revision": "b605524f3752dae26d0d",
+    "url": "/xanxan2/assetclienttest.git/static/js/15.8171ba8d.chunk.js"
+  },
+  {
+    "revision": "264b189c6da7e338d430",
+    "url": "/xanxan2/assetclienttest.git/static/js/16.1d1c5944.chunk.js"
+  },
+  {
+    "revision": "5bf9a2a60f77e769d373",
+    "url": "/xanxan2/assetclienttest.git/static/js/17.952f13d7.chunk.js"
+  },
+  {
+    "revision": "96143bde7f8604d6e886",
+    "url": "/xanxan2/assetclienttest.git/static/js/18.c2cea4ad.chunk.js"
+  },
+  {
+    "revision": "324cca12818e275c6cc6",
+    "url": "/xanxan2/assetclienttest.git/static/js/19.a0863b83.chunk.js"
+  },
+  {
+    "revision": "1ef2f62728f262c520b1",
+    "url": "/xanxan2/assetclienttest.git/static/js/2.d3f4c6b8.chunk.js"
+  },
+  {
+    "revision": "3e9e9e700d5297747105",
+    "url": "/xanxan2/assetclienttest.git/static/js/20.8b1079e6.chunk.js"
+  },
+  {
+    "revision": "0c1b5225d24487917d5e",
+    "url": "/xanxan2/assetclienttest.git/static/js/21.aa5c45f5.chunk.js"
+  },
+  {
+    "revision": "2eb95d64dc1d41be0ba2",
+    "url": "/xanxan2/assetclienttest.git/static/js/22.9b086cd8.chunk.js"
+  },
+  {
+    "revision": "d0aa1e17593c20f4c329",
+    "url": "/xanxan2/assetclienttest.git/static/js/23.bcdfee40.chunk.js"
+  },
+  {
+    "revision": "ed7e1c06c77c825b9cde",
+    "url": "/xanxan2/assetclienttest.git/static/js/24.c2130d9f.chunk.js"
+  },
+  {
+    "revision": "52b364e8ca1658b4b5da",
+    "url": "/xanxan2/assetclienttest.git/static/js/25.9469a06b.chunk.js"
+  },
+  {
+    "revision": "3ebe143d24ae69320995",
+    "url": "/xanxan2/assetclienttest.git/static/js/26.215d9f06.chunk.js"
+  },
+  {
+    "revision": "78e547a09eb9d2fdc1e9",
+    "url": "/xanxan2/assetclienttest.git/static/js/27.667ddb14.chunk.js"
+  },
+  {
+    "revision": "539a6cb1a27b2a92c16b",
+    "url": "/xanxan2/assetclienttest.git/static/js/28.d36ac3e4.chunk.js"
+  },
+  {
+    "revision": "983d63f4338bfaf3b45a",
+    "url": "/xanxan2/assetclienttest.git/static/js/29.17044b7e.chunk.js"
+  },
+  {
+    "revision": "5c9eec7ef1e155724bf1",
+    "url": "/xanxan2/assetclienttest.git/static/js/30.533e0e73.chunk.js"
+  },
+  {
+    "revision": "880661006b34084e420a",
+    "url": "/xanxan2/assetclienttest.git/static/js/31.2d8f56fe.chunk.js"
+  },
+  {
+    "revision": "4c475f2b3e328bac8223",
+    "url": "/xanxan2/assetclienttest.git/static/js/32.9814e346.chunk.js"
+  },
+  {
+    "revision": "f5e3015d38758daddced",
+    "url": "/xanxan2/assetclienttest.git/static/js/33.4da02b69.chunk.js"
+  },
+  {
+    "revision": "7314cb7c7b23eddb64e8",
+    "url": "/xanxan2/assetclienttest.git/static/js/34.ef2dc3fb.chunk.js"
+  },
+  {
+    "revision": "04e79162fcf51a49ee60",
+    "url": "/xanxan2/assetclienttest.git/static/js/35.4f08a906.chunk.js"
+  },
+  {
+    "revision": "743c9c5ab6e73727c410",
+    "url": "/xanxan2/assetclienttest.git/static/js/36.74c5a065.chunk.js"
+  },
+  {
+    "revision": "2bd733bb6b6cd4083415",
+    "url": "/xanxan2/assetclienttest.git/static/js/37.ed666235.chunk.js"
+  },
+  {
+    "revision": "e01942e6a5ffb81c6326",
+    "url": "/xanxan2/assetclienttest.git/static/js/38.62626005.chunk.js"
+  },
+  {
+    "revision": "8e0b5e27ae9983061901",
+    "url": "/xanxan2/assetclienttest.git/static/js/39.8e486cb3.chunk.js"
+  },
+  {
+    "revision": "77d898eafb4b101c9291",
+    "url": "/xanxan2/assetclienttest.git/static/js/40.9ebd8e8c.chunk.js"
+  },
+  {
+    "revision": "bf5d2057a8bba79a140b",
+    "url": "/xanxan2/assetclienttest.git/static/js/41.5bc0ff64.chunk.js"
+  },
+  {
+    "revision": "5a5621a148ebe3274a7f",
+    "url": "/xanxan2/assetclienttest.git/static/js/42.d0ea6d3f.chunk.js"
+  },
+  {
+    "revision": "663084a871f916bdf258",
+    "url": "/xanxan2/assetclienttest.git/static/js/43.f749c6ee.chunk.js"
+  },
+  {
+    "revision": "c3a70e3286dd21347742",
+    "url": "/xanxan2/assetclienttest.git/static/js/44.3655f40c.chunk.js"
+  },
+  {
+    "revision": "e0576b7d80bd22d07f6b",
+    "url": "/xanxan2/assetclienttest.git/static/js/45.cd6af8e4.chunk.js"
+  },
+  {
+    "revision": "88871a0f0f2e853c1274",
+    "url": "/xanxan2/assetclienttest.git/static/js/46.66c609e4.chunk.js"
+  },
+  {
+    "revision": "9283d72602dd41532097",
+    "url": "/xanxan2/assetclienttest.git/static/js/47.728c64f2.chunk.js"
+  },
+  {
+    "revision": "486c14df3f60439d5ef4",
+    "url": "/xanxan2/assetclienttest.git/static/js/48.88c191ac.chunk.js"
+  },
+  {
+    "revision": "a3d4271e05b2df8e03f5",
+    "url": "/xanxan2/assetclienttest.git/static/js/5.014fea45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xanxan2/assetclienttest.git/static/js/5.014fea45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c03e5119e97046a3b4",
+    "url": "/xanxan2/assetclienttest.git/static/js/6.b3fed0bc.chunk.js"
+  },
+  {
+    "revision": "66c11961b0c2224c19d9",
+    "url": "/xanxan2/assetclienttest.git/static/js/7.b79f4b12.chunk.js"
+  },
+  {
+    "revision": "e1cee10b419820d9a0a4",
+    "url": "/xanxan2/assetclienttest.git/static/js/8.7c91ee50.chunk.js"
+  },
+  {
+    "revision": "54623f8de2012d617881",
+    "url": "/xanxan2/assetclienttest.git/static/js/9.99322eca.chunk.js"
+  },
+  {
+    "revision": "e45432fc57de4045c44e",
+    "url": "/xanxan2/assetclienttest.git/static/js/main.b6a31943.chunk.js"
+  },
+  {
+    "revision": "0325bff65e540d86ae0a",
+    "url": "/xanxan2/assetclienttest.git/static/js/runtime-main.238febe6.js"
+  }
+]);
